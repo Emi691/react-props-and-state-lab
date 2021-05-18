8a8f42ea8d60f06f312952fa9845754ea5592638
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-05-18 01:00:41 -0700

    commit tree 6ccd6427601f5af905e558188bd95a4fc7bf5c8e

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,38 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (type) => {
+    this.setState(previousState => {
+      return ({
+        filters:{
+          ...previousState.filters,
+          type: type
+        }
+      })
+    })
+  }
+
+  onFindPetsClick = () => {
+    let queryParam 
+
+    if (this.state.filters.type === 'all') { 
+      queryParam = ""
+    } else {
+      queryParam = `?type=${this.state.filters.type}`
+    }
+
+    fetch(`/api/pets${queryParam}`)
+      .then(resp => resp.json)
+      .then(pets => {
+        this.setState({pets: pets}) 
+      })
+  }
+
+  onAdoptPet = (id) => {
+    let pet = this.state.pets.find(pet => pet.id === id)
+    pet.isAdopted = true
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +56,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType = {this.onChangeType} onFindPetsClick = {this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets = {this.state.pets} onAdoptPet = {this.onAdoptPet}/>
             </div>
           </div>
         </div>
